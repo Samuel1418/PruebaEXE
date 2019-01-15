2c5c95e87cd2eb1ab9dee1ca51482579d12eb97c
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-01-15 09:56:33 +0100

    Licencia

---
+++ ejemploexe/EjemploEXE.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * 
+ * 
+ */
+package ejemploexe;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Samuel
+ */
+public class EjemploEXE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola mundo");
+    }
+    
+}
